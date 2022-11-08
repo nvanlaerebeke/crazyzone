68f125a7b4515e043b7ba1b5a8fa725e5dca04a9
Nico van Laerebeke <nvanlaerebeke@kvm.crazyzone.be>
2022-11-08 07:21:44 +0100

    added gitlab

--- lib/include.sh
+++ lib/include.sh
@@ -1,5 +1,5 @@
 function configure {
-    argocd_install_dir "$ROOT/applications/argocd/cert-manager/"
+    #argocd_install_dir "$ROOT/applications/argocd/cert-manager/"
     argocd_install_dir "$ROOT/applications/argocd/home/"
     argocd_install_dir "$ROOT/applications/argocd/jenkins/"
     argocd_install_dir "$ROOT/applications/argocd/monitoring/"
