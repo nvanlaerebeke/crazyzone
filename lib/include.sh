4946da07a66525e7561959717b5aa94fdcf3b081
Nico van Laerebeke <nvanlaerebeke@kvm.crazyzone.be>
2023-01-29 18:32:07 +0100

    deleted unused power exporter and moved all power related projects to their own namespace

--- lib/include.sh
+++ lib/include.sh
@@ -4,4 +4,5 @@ function configure {
     argocd_install_dir "$ROOT/applications/argocd/jenkins/"
     argocd_install_dir "$ROOT/applications/argocd/monitoring/"
     argocd_install_dir "$ROOT/applications/argocd/operations/"
+    argocd_install_dir "$ROOT/applications/argocd/power/"
 }
