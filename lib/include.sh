d70d2fd45e79bc65633ccdd212cf4596bdbdf551
Nico van Laerebeke <nvanlaerebeke@kvm.crazyzone.be>
2023-02-08 11:42:03 +0100

    fixed namespace for the jenkins-nmd volume claim

--- lib/include.sh
+++ lib/include.sh
@@ -2,6 +2,7 @@ function configure {
     argocd_install_dir "$ROOT/applications/argocd/cert-manager/"
     argocd_install_dir "$ROOT/applications/argocd/home/"
     argocd_install_dir "$ROOT/applications/argocd/jenkins/"
+    argocd_install_dir "$ROOT/applications/argocd/jenkins-nmd/"
     argocd_install_dir "$ROOT/applications/argocd/monitoring/"
     argocd_install_dir "$ROOT/applications/argocd/operations/"
     argocd_install_dir "$ROOT/applications/argocd/power/"
