091908d869b5e6b852a06f34db5baa52a280a940
Nico Van Laerebeke <nico.vanlaerebeke@gmail.com>
2025-08-29 18:27:54 +0200

    Added include entry

--- lib/include.sh
+++ lib/include.sh
@@ -7,7 +7,9 @@ function configure {
     #argocd_install_dir "$ROOT/applications/argocd/monitoring/"
     #argocd_install_dir "$ROOT/applications/argocd/operations/"
     #argocd_install_dir "$ROOT/applications/argocd/power/"
-    argocd_install_dir "$ROOT/applications/argocd/registry/"
+    #argocd_install_dir "$ROOT/applications/argocd/registry/"
     #argocd_install_dir "$ROOT/applications/argocd/rancher/"
     #argocd_install_dir "$ROOT/applications/argocd/unifi/"
+
+    argocd_install_application "$ROOT/applications/argocd/home/torrent.yaml"
 }
