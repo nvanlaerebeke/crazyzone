40a0bb5827f813dd2eb98ef8e2da4eccb4fb6a8b
Nico Van Laerebeke <nico.vanlaerebeke@gmail.com>
2025-10-10 19:22:59 +0200

    enabled argocd-server-tls

--- lib/include.sh
+++ lib/include.sh
@@ -1,4 +1,6 @@
 function configure {
+    argocd_install_application "$ROOT/applications/argocd/core/argocd.yaml"
+
     #argocd_install_dir "$ROOT/applications/argocd/backend-services/"
     #argocd_install_dir "$ROOT/applications/argocd/cert-manager/"
     #argocd_install_dir "$ROOT/applications/argocd/home/"
@@ -11,5 +13,5 @@ function configure {
     #argocd_install_dir "$ROOT/applications/argocd/rancher/"
     #argocd_install_dir "$ROOT/applications/argocd/unifi/"
 
-    argocd_install_application "$ROOT/applications/argocd/home/torrent.yaml"
+    #argocd_install_application "$ROOT/applications/argocd/home/torrent.yaml"
 }
