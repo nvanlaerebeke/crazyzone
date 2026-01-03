51e3c6159db4c86b4e4f5d03dc5bfc4640a93c57
Nico Van Laerebeke <nico.vanlaerebeke@gmail.com>
2026-01-03 22:35:01 +0100

    setting the current image tag is different now

--- lib/include.sh
+++ lib/include.sh
@@ -3,7 +3,7 @@ function configure {
     #argocd_install_application "$ROOT/applications/argocd/operations/reflector.yaml"
 
     #argocd_install_application "$ROOT/applications/argocd/backend-services/minio.yaml"
-    argocd_install_application "$ROOT/applications/argocd/backend-services/postgresql.yaml"
+    #argocd_install_application "$ROOT/applications/argocd/backend-services/postgresql.yaml"
     #argocd_install_application "$ROOT/applications/argocd/backend-services/postgresql-new.yaml"
     #argocd_install_application "$ROOT/applications/argocd/backend-services/redis.yaml"
 
@@ -15,7 +15,7 @@ function configure {
     #argocd_install_application "$ROOT/applications/argocd/home/crazy-store.yaml"
     #argocd_install_application "$ROOT/applications/argocd/home/dashboard.yaml"
     #argocd_install_application "$ROOT/applications/argocd/home/host.yaml"
-    #argocd_install_application "$ROOT/applications/argocd/home/immich.yaml"
+    argocd_install_application "$ROOT/applications/argocd/home/immich.yaml"
     #argocd_install_application "$ROOT/applications/argocd/home/torrent.yaml"
 
     #argocd_install_application "$ROOT/applications/argocd/jenkins/jenkins.yaml"
